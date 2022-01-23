98e98e83be193935e3e02d89581c70e16eac6f73
root <root@vultr.guest>
2022-01-23 22:33:03 +0000

    checkpoint

--- frontend/server.js
+++ frontend/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const port = 80;
 const fs = require('fs');
+const path = require('path');
 
 app.use(express.static('src'));
 
 app.get("/", (req, res) => {
-	sendFile();
+	res.sendFile(path.join(__dirname, '/index.html'));
+	return false;
+});
+
+app.post("", (req, res) => {
+	
 });
 
 app.listen(port);
---
+++ frontend/src/script.js
@@ -0,0 +1,13 @@
+function register() {
+	const employee = {};
+	employee.registerNumber = document.getElementById("regnumber").value;
+	employee.name = document.getElementById("name").value;
+	employee.branch = document.getElementById("branch").value;
+	employee.email = document.getElementById("email").value;
+	employee.sector = document.getElementById("sector").value;
+	employee.birthdate = document.getElementById("birthdate").value;
+	console.log(employee);
+	/*fetch()
+	.then()
+	.then()*/
+}
